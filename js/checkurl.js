e648755c4f271eb1c6f7e887ed9fe9c7b872bd99
Hidenori Miyoshi <miyohide@gmail.com>
2018-03-31 00:04:12 +0900

    add notice message.

--- js/checkurl.js
+++ js/checkurl.js
@@ -10,6 +10,9 @@ if (match_result) {
         // 各号の表紙
         url = "articles/" + match_result[1] + "/" + match_result[1] + "-index.html";
     }
+    document.getElementById("redirect_notice").innerHTML = "http://magazine.rubyist.net/" + query_string + "はhttp://magazine.rubyist.net/" + url + "に移行しました。<br />ブックマーク等の変更をお願いします。";
+    document.getElementById("redirect_notice").style.color = "white";
+    document.getElementById("redirect_notice").style.backgroundColor = "red";
     setTimeout("redirect_newpage('" + url + "')", 500);
 }
 
