129990f3958c36594a5f1148377d626803a67d20
Mitsutaka Mimura <takkanm@gmail.com>
2023-06-11 22:01:37 +0900

    ルールの定数一覧をモジュールにいれた

--- editing_tools/rubima-lint.rb
+++ editing_tools/rubima-lint.rb
@@ -1,45 +1,49 @@
 #!/usr/bin/env ruby
 
 class RubimaLint
-  NOT_ASCII  = '[^[:ascii:]]'     # 非ASCII
-  ASCII_CHAR = '[\w&&[:ascii:]]'  # ASCII
-  OPEN_PARENTHESES  = '[(]'  # 開き丸括弧
-  CLOSE_PARENTHESES = '[)]'  # 閉じ丸括弧
-  QUESTION_EXCLAMATION = '[？！]'  # 疑問符・感嘆符
-  PUNCTUATION = "[、。#{QUESTION_EXCLAMATION}]"  # 句読点
-  OPEN_BRANCKETS  = '[「『]'  # 開き括弧類
-  CLOSE_BRANCKETS = '[』」]'  # 閉じ括弧類
-  THREE_POINT_LEADER = '[…]'  # 三点リーダ
-  OTHER_OK_LETTER = "[#{THREE_POINT_LEADER}〜：　]"  # その他OK文字
-  OK_BEFORE_ASCII = "[#{PUNCTUATION}#{OPEN_BRANCKETS}#{OTHER_OK_LETTER}]"  # ASCII直前のOK文字
-  OK_AFTER_ASCII = "[#{PUNCTUATION}#{CLOSE_BRANCKETS}#{OTHER_OK_LETTER}]"  # ASCII直後のOK文字
-  HEAD_CHAR = "'''　"  # 発言頭
-  ASCII_AFTER_NOT_ASCII = /(?<=#{NOT_ASCII})(?=#{ASCII_CHAR})(?<!#{OK_BEFORE_ASCII})(?<!#{HEAD_CHAR})/o  # 非ASCIIの直後にASCII
-    NOT_ASCII_AFTER_ASCII = /(?<=#{ASCII_CHAR})(?=#{NOT_ASCII})(?!#{OK_AFTER_ASCII})/o  # ASCIIの直後に非ASCII
-    MISSING_BLANK = /#{ASCII_AFTER_NOT_ASCII}|#{NOT_ASCII_AFTER_ASCII}/o  # 空白抜け
+  module Rules
+    NOT_ASCII  = '[^[:ascii:]]'     # 非ASCII
+    ASCII_CHAR = '[\w&&[:ascii:]]'  # ASCII
+    OPEN_PARENTHESES  = '[(]'  # 開き丸括弧
+    CLOSE_PARENTHESES = '[)]'  # 閉じ丸括弧
+    QUESTION_EXCLAMATION = '[？！]'  # 疑問符・感嘆符
+    PUNCTUATION = "[、。#{QUESTION_EXCLAMATION}]"  # 句読点
+    OPEN_BRANCKETS  = '[「『]'  # 開き括弧類
+    CLOSE_BRANCKETS = '[』」]'  # 閉じ括弧類
+    THREE_POINT_LEADER = '[…]'  # 三点リーダ
+    OTHER_OK_LETTER = "[#{THREE_POINT_LEADER}〜：　]"  # その他OK文字
+    OK_BEFORE_ASCII = "[#{PUNCTUATION}#{OPEN_BRANCKETS}#{OTHER_OK_LETTER}]"  # ASCII直前のOK文字
+    OK_AFTER_ASCII = "[#{PUNCTUATION}#{CLOSE_BRANCKETS}#{OTHER_OK_LETTER}]"  # ASCII直後のOK文字
+    HEAD_CHAR = "'''　"  # 発言頭
+    ASCII_AFTER_NOT_ASCII = /(?<=#{NOT_ASCII})(?=#{ASCII_CHAR})(?<!#{OK_BEFORE_ASCII})(?<!#{HEAD_CHAR})/o  # 非ASCIIの直後にASCII
+      NOT_ASCII_AFTER_ASCII = /(?<=#{ASCII_CHAR})(?=#{NOT_ASCII})(?!#{OK_AFTER_ASCII})/o  # ASCIIの直後に非ASCII
+      MISSING_BLANK = /#{ASCII_AFTER_NOT_ASCII}|#{NOT_ASCII_AFTER_ASCII}/o  # 空白抜け
 
-  # 円括弧前後OK文字
-  OK_AROUND_PARENTHESES = "[ [[:ascii:]&&[:graph:]]#{PUNCTUATION}#{OPEN_BRANCKETS}#{CLOSE_BRANCKETS}#{THREE_POINT_LEADER}]"
+    # 円括弧前後OK文字
+    OK_AROUND_PARENTHESES = "[ [[:ascii:]&&[:graph:]]#{PUNCTUATION}#{OPEN_BRANCKETS}#{CLOSE_BRANCKETS}#{THREE_POINT_LEADER}]"
 
-  union = [
-    /(?<!^)(?<!#{HEAD_CHAR})(?<!#{OK_AROUND_PARENTHESES})#{OPEN_PARENTHESES}/o,
-    /#{CLOSE_PARENTHESES}(?!#{OK_AROUND_PARENTHESES})(?!$)/o,
-    PARENTHESES_AT_END_OF_STATE = /。#{CLOSE_PARENTHESES}。/o,  # 文末で括弧を閉じる場合
-      LAUGH_AND_PARENTHESES = /笑#{CLOSE_PARENTHESES}。$/o,    # 括弧笑の後の句点
-      SINGLE_THREE_POINT_LEADER = /(?<!#{THREE_POINT_LEADER})#{THREE_POINT_LEADER}(?!#{THREE_POINT_LEADER})/o,  # 単独の三点リーダ
-      THREE_POINT_LEADER_AT_END = /#{THREE_POINT_LEADER}$/o,  # 文末の三点リーダ
-    QUESTION_EXCLAMATION_IN_PARAGRAPH = /#{QUESTION_EXCLAMATION}(?!$)(?![　#{QUESTION_EXCLAMATION}#{CLOSE_BRANCKETS}])/o,  # 段落中の疑問符・感嘆符
-    FULL_WIDTH_PARENTHESES = /[（）]/o,  # 全角括弧
-  ]
-  INVALID_PATTERN = Regexp.union(*union)
+    union = [
+      /(?<!^)(?<!#{HEAD_CHAR})(?<!#{OK_AROUND_PARENTHESES})#{OPEN_PARENTHESES}/o,
+      /#{CLOSE_PARENTHESES}(?!#{OK_AROUND_PARENTHESES})(?!$)/o,
+      PARENTHESES_AT_END_OF_STATE = /。#{CLOSE_PARENTHESES}。/o,  # 文末で括弧を閉じる場合
+        LAUGH_AND_PARENTHESES = /笑#{CLOSE_PARENTHESES}。$/o,    # 括弧笑の後の句点
+        SINGLE_THREE_POINT_LEADER = /(?<!#{THREE_POINT_LEADER})#{THREE_POINT_LEADER}(?!#{THREE_POINT_LEADER})/o,  # 単独の三点リーダ
+        THREE_POINT_LEADER_AT_END = /#{THREE_POINT_LEADER}$/o,  # 文末の三点リーダ
+      QUESTION_EXCLAMATION_IN_PARAGRAPH = /#{QUESTION_EXCLAMATION}(?!$)(?![　#{QUESTION_EXCLAMATION}#{CLOSE_BRANCKETS}])/o,  # 段落中の疑問符・感嘆符
+      FULL_WIDTH_PARENTHESES = /[（）]/o,  # 全角括弧
+    ]
+    INVALID_PATTERN = Regexp.union(*union)
 
-  CHAR_AROUND_NOT_PERMIT_BLANK = "[#{PUNCTUATION}#{OPEN_BRANCKETS}#{CLOSE_BRANCKETS}]"
-  union = [
-    /#{THREE_POINT_LEADER} #{OPEN_PARENTHESES}/o,
-    /#{CHAR_AROUND_NOT_PERMIT_BLANK} #{OPEN_PARENTHESES}/o,
-    /#{CLOSE_PARENTHESES} #{CHAR_AROUND_NOT_PERMIT_BLANK}/o,
-  ]
-  INVALID_BLANK = Regexp.union(*union)
+    CHAR_AROUND_NOT_PERMIT_BLANK = "[#{PUNCTUATION}#{OPEN_BRANCKETS}#{CLOSE_BRANCKETS}]"
+    union = [
+      /#{THREE_POINT_LEADER} #{OPEN_PARENTHESES}/o,
+      /#{CHAR_AROUND_NOT_PERMIT_BLANK} #{OPEN_PARENTHESES}/o,
+      /#{CLOSE_PARENTHESES} #{CHAR_AROUND_NOT_PERMIT_BLANK}/o,
+    ]
+    INVALID_BLANK = Regexp.union(*union)
+  end
+
+  include Rules
 
   attr_reader :warning_count, :error_messages
 
